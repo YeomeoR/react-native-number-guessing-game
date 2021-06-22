73b73df0729e7e965e2f1a44b260cea049bfe402
YeomeoR <YeomeoR@users.noreply.github.com>
2021-06-22 16:50:59 +0100

    first commit

---
+++ TemplateComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const functionalComponentName = (props) => {
+  return <div></div>;
+};
+
+// styles
+const styles = StyleSheet.create({});
+
+export default functionalComponentName;
